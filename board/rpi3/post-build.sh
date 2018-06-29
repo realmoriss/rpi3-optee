fb32242f1b9a67e5520fcf8bb01dc79b879b71d8
Istvan Telek <telek96+github@gmail.com>
2018-06-29 10:58:45 +0200

    Auto generate SSH host keys during build

--- board/rpi3/post-build.sh
+++ board/rpi3/post-build.sh
@@ -1,1 +1,19 @@
 #!/bin/sh
+
+KEY_PATH="${BR2_EXTERNAL_RPI3_OPTEE_PATH}/board/rpi3/rootfs-overlay/etc/ssh"
+
+if [ ! -f "${KEY_PATH}/ssh_host_rsa_key" ]; then
+	ssh-keygen -t rsa -b 2048 -f "${KEY_PATH}/ssh_host_rsa_key" -N ""
+fi
+
+if [ ! -f "${KEY_PATH}/ssh_host_dsa_key" ]; then
+	ssh-keygen -t dsa -b 1024 -f "${KEY_PATH}/ssh_host_dsa_key" -N ""
+fi
+
+if [ ! -f "${KEY_PATH}/ssh_host_ecdsa_key" ]; then
+	ssh-keygen -t ecdsa -b 256 -f "${KEY_PATH}/ssh_host_ecdsa_key" -N ""
+fi
+
+if [ ! -f "${KEY_PATH}/ssh_host_ed25519_key" ]; then
+	ssh-keygen -t ed25519 -b 2048 -f "${KEY_PATH}/ssh_host_ed25519_key" -N ""
+fi

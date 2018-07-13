099e05c1ae79a6e1f3abeae6e99a3661cabb29ba
Istvan Telek <telek96+github@gmail.com>
2018-07-13 13:12:59 +0200

    Add getty to primary console output

--- board/rpi3/post-build.sh
+++ board/rpi3/post-build.sh
@@ -22,3 +22,10 @@ if [ ! -f "${KEY_PATH}/ssh_host_ed25519_key" ]; then
 	ssh-keygen -t ed25519 -b 2048 -f "${KEY_PATH}/ssh_host_ed25519_key" -N ""
 	cp ${KEY_PATH}/ssh_host_ed25519_key ${TARGET_KEY_PATH}
 fi
+
+GETTY_LINE="tty1::respawn:/sbin/getty -L tty1 0 xterm-256color"
+INITTAB_PATH="${TARGET_DIR}/etc/inittab"
+
+if ! grep -q ${GETTY_LINE} ${INITTAB_PATH}; then
+	echo ${GETTY_LINE} >> ${INITTAB_PATH}
+fi
